84540da6ab90b8ece194b30dce751832cdad5355
azitowolf <azito.wolf3@gmail.com>
2017-08-08 13:49:56 +0800

    update script add fields

--- horseman.js
+++ horseman.js
@@ -25,7 +25,15 @@ var scrapeDatPage = function(id, endId) {
             sqm: 0,
             beds: 0,
             baths: 0,
-            description: ""  
+            description: "",
+            dateAdded: {
+                day: 0,
+                month: 0,
+                year: 0
+            },
+            compound: "",
+            floor: "",
+            buildingType: ""
         }   
 
         horseman
@@ -50,25 +58,14 @@ var scrapeDatPage = function(id, endId) {
             .then(function (address){
                 houseDataModel.address = address
             })  
-            // .then(function(){ // return if this is an empty listing
-            //     if(houseDataModel.address === "") {
-            //         console.log("found empty listing");
-            //         houseDataModel = {
-            //             id:id,
-            //             emptyListing: true                        
-            //         }
-            //         resolve(houseDataModel); 
-            //         fs.appendFile('output.json', JSON.stringify(houseDataModel), function() {
-            //             console.log('appended contents to output.json');
-            //         });                         
-            //         horseman.close();
-            //         return stepToNextListing(id, endId);
-            //     }
-            // })   
+
             .html(".jieshao_je") // pulling rent
             .then(function (rent){
                 houseDataModel.rent = rent
             })
+
+            // pulling from main info section
+
             .html(".xiaoqu_mingxi") // pulling district
             .then(function (html){
                 houseDataModel.district = html.split('<span>')[0];
@@ -93,11 +90,48 @@ var scrapeDatPage = function(id, endId) {
                 if(stringifiedHTML.match(/(\d{1,2})(?=卫)/g)) {
                     houseDataModel.baths = stringifiedHTML.match(/(\d{1,2})(?=卫)/g)[0];
                 }                 
-            })   
+            })  
+            .html(".xiaoqu_mingxi") // pulling floor
+            .then(function (html){
+                var stringifiedHTML = html.toString();
+                if(stringifiedHTML.match(/第(.*)层/g)) {
+                    houseDataModel.floor = stringifiedHTML.match(/第(.*)层/g)[0];
+                }                 
+            })
+            
+            // end main info section
+
             .html(".xiangqing") // pulling description
             .then(function (html){
                 houseDataModel.description = html
-            })   
+            }) 
+            .html(".xiaoqu_name") // pulling buildingType
+            .then(function (html){
+                htmlContentArray = html.split('<span>');
+                houseDataModel.buildingType = htmlContentArray[0];                
+            })
+            .html(".xiaoqu_name") // pulling compound
+            .then(function (html){
+                var stringifiedHTML = html.toString();
+                if(stringifiedHTML.match(/<span>(.*)<\/span>/g)) {
+                    compoundStr = stringifiedHTML.match(/<span>(.*)<\/span>/g)[0];
+                    compoundStrMinusSpans = compoundStr.replace(/<\/?span[^>]*>/g,"");
+                    houseDataModel.compound = compoundStrMinusSpans;
+                }                  
+            })                              
+            .html(".addtime") // pulling dateAdded
+            .then(function (html){
+                var stringifiedHTML = html.toString();
+                if(stringifiedHTML.match(/(\d{2})(?=日)/g)){                    
+                    houseDataModel.dateAdded.day = parseInt(stringifiedHTML.match(/(\d{2})(?=日)/g)[0])
+                }
+                if(stringifiedHTML.match(/(\d{2})(?=月)/g)){                    
+                    houseDataModel.dateAdded.month = parseInt(stringifiedHTML.match(/(\d{2})(?=月)/g)[0])
+                }   
+                if(stringifiedHTML.match(/(\d{4})(?=年)/g)){                    
+                    houseDataModel.dateAdded.year = parseInt(stringifiedHTML.match(/(\d{4})(?=年)/g)[0])
+                }                                                                   
+            })                     
             .html(".jieshao_phone") // pulling landlord phone
             .then(function(agentPhone){
                 houseDataModel.agent.phone = agentPhone
@@ -113,8 +147,7 @@ var scrapeDatPage = function(id, endId) {
                     houseDataModel.agent.type = "landlord";
                 } else {
                     houseDataModel.agent.type = "agent";
-                }
-            
+                }            
             })              
             .then(function(){                
                 fs.appendFile('output.json', JSON.stringify(houseDataModel), function(err) {
@@ -173,6 +206,23 @@ rl.question('Starting Id ', (answer) => {
     });
 });
 
+// USE FOR LISTING SKIPPING IF NEEDED 
+
+// .then(function(){ // return if this is an empty listing
+//     if(houseDataModel.address === "") {
+//         console.log("found empty listing");
+//         houseDataModel = {
+//             id:id,
+//             emptyListing: true                        
+//         }
+//         resolve(houseDataModel); 
+//         fs.appendFile('output.json', JSON.stringify(houseDataModel), function() {
+//             console.log('appended contents to output.json');
+//         });                         
+//         horseman.close();
+//         return stepToNextListing(id, endId);
+//     }
+// })   
 
 
 
